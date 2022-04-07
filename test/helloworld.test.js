5b23bba5414d3f16d24a4a74906b39f3f1d032c5
Diego Fornalha <diegofornalha@gmail.com>
2022-04-07 11:50:30 -0300

    segunda tentativa de teste

--- test/helloworld.test.js
+++ test/helloworld.test.js
@@ -2,6 +2,9 @@ const HelloWorld = artifacts.require("HelloWorld");
 
 contract("HelloWorld", function (accounts) {
   beforeEach(async () => {
+    contract = await HelloWorld.new();
+  });
+  it("should return the correct message", async () => {
 const res = await contract.greetings();
 assert(res == "Hello World!");
   });
