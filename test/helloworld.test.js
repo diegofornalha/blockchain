b6716bea5d51f62eabbd46f5fd4c1eb700f04018
Diego Fornalha <diegofornalha@gmail.com>
2022-04-07 11:58:11 -0300

    6 tentativa de teste

--- test/helloworld.test.js
+++ test/helloworld.test.js
@@ -4,7 +4,7 @@ contract("HelloWorld", function (accounts) {
   beforeEach(async () => {
     contract = await HelloWorld.new();
   });
-  it("should return the correct message", async () => {
+  it('need show greetings', async () => {
   const res = await contract.greetings();
 
   assert(res === "Ola semana solidity!");
