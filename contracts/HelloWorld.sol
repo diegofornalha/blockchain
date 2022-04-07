0370e2051b400a9c63f1075b2bd343c88843c75c
Diego Fornalha <diegofornalha@gmail.com>
2022-04-07 11:01:59 -0300

    contrato criado

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.8.0;
+
+/// @title A title that should describe the contract/interface
+/// @author The name of the author
+/// @notice Explain to an end user what this does
+/// @dev Explain to a developer any extra details
+
+contract HelloWorld {
+    string public message = "Hello World!";
+    function print() public view returns (string memory) {
+        return message;
+    }
+}
